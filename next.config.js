487f72ad4e82f303feb53dc028b8e9ecaa81f031
Sergey Moraru <54947469+celishere@users.noreply.github.com>
2025-01-31 00:47:57 +0300

    don`t ignore build errors

--- next.config.js
+++ next.config.js
@@ -2,14 +2,8 @@ const IS_DEV = false;
 
 /** @type {import('next').NextConfig, {webpack(*, *): (*), exportPathMap: function(...[*]): Promise<*>}} */
 const nextConfig = {
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
     experimental: {
         reactCompiler: true,
-    },
-    typescript: {
-        ignoreBuildErrors: true,
     },
     env: {
         API_URL: !IS_DEV ? "YOUR_HOST" : "http://localhost:4019",
